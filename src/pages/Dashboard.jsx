3aa6b24ffe42cb993bc276cccbc570e3e9ee28b3
Gabriel Sanches <gabrielsanches131202@gmail.com>
2025-02-27 22:32:24 -0300

    fix: ajustando o posicionamento dos componentes do dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,27 +1,26 @@
-function Dashboard() {
-    return (
-      <div>
-        <h1 className="text-3xl font-bold text-gray-500">Painel Financeiro</h1>
-        
-        <div className="grid grid-cols-3 gap-4">
-            <div className="bg-white p-6 rounded-lg shadow-md">
-                <h2 className="text-black text-x1 font-semibold">Saldo Atual</h2>
-                <p className="text-green-500 text-2x1">R$ 5.000,00</p>
-            </div>
+export default function Dashboard() {
+  return (
+    <div className="w-screen ml-5 mr-5">
+      <h1 className="text-3xl font-bold text-gray-500 pt-5 pb-5">
+        Painel Financeiro
+      </h1>
 
-            <div className="bg-white p-6 rounded-lg shadow-md">
-                <h2 className="text-black text-x1 font-semibold">Receitas</h2>
-                <p className="text-blue-500 text-2x1">R$ 8.000,00</p>
-            </div>
+      <div className="grid grid-cols-3 gap-4">
+        <div className="bg-white p-6 rounded-lg shadow-md">
+          <h2 className="text-black text-x1 font-semibold">Saldo Atual</h2>
+          <p className="text-green-500 text-2x1">R$ 5.000,00</p>
+        </div>
 
-            <div className="bg-white p-6 rounded-lg shadow-md">
-                <h2 className="text-black text-x1 font-semibold">Despesas</h2>
-                <p className="text-red-500 text-2x1">R$ 3.000,00</p>
-            </div>
+        <div className="bg-white p-6 rounded-lg shadow-md">
+          <h2 className="text-black text-x1 font-semibold">Receitas</h2>
+          <p className="text-blue-500 text-2x1">R$ 8.000,00</p>
+        </div>
+
+        <div className="bg-white p-6 rounded-lg shadow-md">
+          <h2 className="text-black text-x1 font-semibold">Despesas</h2>
+          <p className="text-red-500 text-2x1">R$ 3.000,00</p>
         </div>
       </div>
-    );
-  }
-  
-  export default Dashboard;
-  
+    </div>
+  );
+}
